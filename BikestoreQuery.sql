39051fca7a4bbe3949f9c6b1a465202a36a88230
nithin790 <113250052+nithin790@users.noreply.github.com>
2023-05-16 15:23:23 -0500

    Add brand name and built dashboard

--- BikestoreQuery.sql
+++ BikestoreQuery.sql
@@ -1,26 +1,25 @@
-/*You can download the raw data for this link (github user's)
-	link:- http://www.sqlservertutorial.net/load-sample-database/
-	*/
+
 
 USE BIkeStores;
+
 SELECT ord.order_id,
        CONCAT(cus.first_name, ' ', cus.last_name) AS 'customer',
        cus.city,
        cus.state,
        ord.order_date,
        SUM(ite.quantity) AS total_units,
-       SUM(ite.quantity) AS revenue,
+       SUM(ite.quantity * ite.list_price) AS 'revenue',
        pro.product_name,
-	   cat.category_name,
-	   sto.store_name,
-	   CONCAT(sta.first_name, ' ', sta.last_name) AS 'sales_rep'
+       cat.category_name,
+       sto.store_name,
+       CONCAT(sta.first_name, ' ', sta.last_name) AS 'sales_rep'
 FROM sales.orders ord
 JOIN sales.customers cus ON ord.customer_id = cus.customer_id
 JOIN sales.order_items ite ON ord.order_id = ite.order_id
 JOIN production.products pro ON ite.product_id = pro.product_id
-join production.categories cat On pro.category_id =cat.category_id
-join sales.stores sto On ord.store_id =sto.store_id
-join sales.staffs sta on ord.staff_id =sta.staff_id
+JOIN production.categories cat ON pro.category_id = cat.category_id
+JOIN sales.stores sto ON ord.store_id = sto.store_id
+JOIN sales.staffs sta ON ord.staff_id = sta.staff_id
 GROUP BY
        ord.order_id,
        CONCAT(cus.first_name, ' ', cus.last_name),
@@ -28,8 +27,8 @@ GROUP BY
        cus.state,
        ord.order_date,
        pro.product_name,
-	   cat.category_name,
-	   sto.store_name,
-	   CONCAT(sta.first_name, ' ', sta.last_name) 
+       cat.category_name,
+       sto.store_name,
+       CONCAT(sta.first_name, ' ', sta.last_name);
 
 
